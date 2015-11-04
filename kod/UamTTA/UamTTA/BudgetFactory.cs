3fd559527ea1e22b1a153189c485b75ec94a45b6
Deinonzch <slagol@st.amu.edu.pl>
2015-11-04 19:08:47 +0100

    ready

--- kod/UamTTA/UamTTA/BudgetFactory.cs
+++ kod/UamTTA/UamTTA/BudgetFactory.cs
@@ -18,9 +18,11 @@ namespace UamTTA
                     break;
 
                 case Duration.Quarterly:
+                    endDate = AddQuarterly(startDate);
                     break;
 
                 case Duration.Yearly:
+                    endDate = AddYearly(startDate);
                     break;
 
                 default:
@@ -42,6 +44,24 @@ namespace UamTTA
             int daysInNextMonth = DateTime.DaysInMonth(endDate.Year, endDate.Month);
             if (daysInNextMonth >= 30 && (endDate.Day < daysInNextMonth || daysInNextMonth == daysInStartDate))
                 endDate = endDate.AddDays(-1);
+            return endDate;
+        }
+
+        private static DateTime AddQuarterly(DateTime startDate)
+        {
+            DateTime endDate = startDate.AddMonths(2);
+            int daysInSecondMonth = DateTime.DaysInMonth(endDate.Year, endDate.Month);
+            endDate = endDate.AddDays(daysInSecondMonth - 1);
+            return endDate;
+        }
+
+        private static DateTime AddYearly(DateTime startDate)
+        {
+            DateTime endDate = startDate;
+            if ((((startDate.Year % 4 == 0 && startDate.Year % 100 != 0) || startDate.Year % 400 == 0) && startDate.Month <= 2) || ((((startDate.Year + 1) % 4 == 0 && (startDate.Year + 1) % 100 != 0) || (startDate.Year + 1) % 400 == 0)) && startDate.Month >= 3)
+                endDate = endDate.AddDays(365);
+            else
+                endDate = endDate.AddDays(364);
             return endDate;
         }
     }
